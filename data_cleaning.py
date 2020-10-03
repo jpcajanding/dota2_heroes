610f027db4bedc109e1a9c68f1efab641daa3f3d
jpcajanding <jpmcajanding@gmail.com>
2020-10-04 01:53:33 +0800

    Created datasets for analysis and visualization

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import requests
+import numpy as np
+import pandas as pd
 
 pro_matches_heroes = pd.read_pickle('data\pro_matches_heroes.pkl')
 
@@ -41,6 +42,7 @@ def parse_heroes(matches, radiant_win, patch):
 patch45_heroes = parse_heroes(pro_matches_heroes_patch45[pro_matches_heroes_patch45['radiant_win']], radiant_win=True, patch=45).append(parse_heroes(pro_matches_heroes_patch45[~pro_matches_heroes_patch45['radiant_win']], radiant_win=False, patch=45), ignore_index=True)
 patch46_heroes = parse_heroes(pro_matches_heroes_patch46[pro_matches_heroes_patch46['radiant_win']], radiant_win=True, patch=46).append(parse_heroes(pro_matches_heroes_patch46[~pro_matches_heroes_patch46['radiant_win']], radiant_win=False, patch=46), ignore_index=True)
 
+# these datasets are for classification
 patch45_heroes.to_pickle('data\pro_heroes_patch_45.pkl')
 patch46_heroes.to_pickle('data\pro_heroes_patch_46.pkl')
 
@@ -65,3 +67,32 @@ hero_names_46 = parse_hero_names(patch46_heroes, heroes)
 
 hero_names_45.to_csv('data\hero_names_45.csv')
 hero_names_46.to_csv('data\hero_names_46.csv')
+
+
+def get_hero_cross_tabulation(hero_match_results, heroes):
+    win_columns = [column for column in hero_names_45.columns if 'win' in column]
+    loss_columns = [column for column in hero_names_45.columns if 'loss' in column]
+
+    heroes['losses'] = heroes['localized_name'].map(hero_match_results[loss_columns].melt()['value'].value_counts())
+    heroes['wins'] = heroes['localized_name'].map(hero_match_results[win_columns].melt()['value'].value_counts())
+    heroes['picks'] = heroes['localized_name'].map(hero_match_results.melt()['value'].value_counts())
+
+    for hero in heroes['localized_name']:
+        hero_lost_matches = hero_match_results[(hero_match_results[loss_columns].values == hero).any(1)]
+        heroes['win_against_' + hero] = heroes['localized_name'].map(hero_lost_matches[win_columns].melt()['value'].value_counts())
+        heroes['loss_with_' + hero] = heroes['localized_name'].map(hero_lost_matches[loss_columns].melt()['value'].value_counts())
+        hero_won_matches = hero_match_results[(hero_match_results[win_columns].values == hero).any(1)]
+        heroes['win_with_' + hero] = heroes['localized_name'].map(hero_lost_matches[win_columns].melt()['value'].value_counts())
+        heroes['loss_against_' + hero] = heroes['localized_name'].map(hero_lost_matches[loss_columns].melt()['value'].value_counts())
+
+    return heroes
+
+
+heroes_p45 = get_hero_cross_tabulation(hero_names_45, heroes)
+heroes_p46 = get_hero_cross_tabulation(hero_names_46, heroes)
+
+print(heroes_p45.shape)
+print(heroes_p46.shape)
+
+heroes_p45.to_csv('data\heroes_patch_45.csv')
+heroes_p46.to_csv('data\heroes_patch_46.csv')
